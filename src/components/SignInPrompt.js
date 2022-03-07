0ff3616a594f587cfc9c6293f98a4afdbb07e871
Nicole Avison <nicole.avison@salecycle.com>
2022-03-07 18:53:45 +0000

    fix: allow SignInPrompt to render

--- src/components/SignInPrompt.js
+++ src/components/SignInPrompt.js
@@ -2,7 +2,7 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { FirebaseUiConfig } from '../modules/firebaseuiConfig';
 
 export const SignInPrompt = ({ firebase }) => {
-  <div>
+  return <div>
     <h1>Riding-Tracker</h1>
     <p>Please sign-in:</p>
     <StyledFirebaseAuth uiConfig={FirebaseUiConfig} firebaseAuth={firebase.auth()} />
