5288d8377a57dbb97c05faaddc896a49a2f783d9
Nicole Avison <nicole@avison.co.uk>
2022-03-31 14:31:41 +0100

    actually saving the files...

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,15 @@
-import logo from '../assets/logo.svg';
+import { SelectActivity } from './SelectActivity';
 
 export const Header = ({userName}) => {
   return <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
     <p>
-      Hello {userName}.
+      Hello {userName}!
     </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
+    <p>
+      What did you do today?
+    </p>
+
+    <SelectActivity />
+
   </header>
 }
